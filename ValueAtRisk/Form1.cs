6ed74d9676c5aba7a8aa4d326e9f57ce80398194
csepanyij <csepanyij@gmail.com>
2017-12-05 22:42:53 +0100

    3. feladat

--- ValueAtRisk/Form1.cs
+++ ValueAtRisk/Form1.cs
@@ -12,9 +12,17 @@ namespace ValueAtRisk
 {
     public partial class Form1 : Form
     {
+        List<Tick> Ticks;
+        AdatbazisDataContext db = new AdatbazisDataContext();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ticks = db.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
